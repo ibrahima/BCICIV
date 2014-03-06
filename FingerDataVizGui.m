3e504f3fa82807c7c5799cb298a6ac10f07ea0bc
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-03-06 12:45:06 -0800

    Make power spectrum GUI a lot nicer.

--- FingerDataVizGui.m
+++ FingerDataVizGui.m
@@ -1,4 +1,4 @@
-classdef FingerDataVizGui
+classdef FingerDataVizGui < handle
     properties
         train_data
         train_dg
@@ -7,13 +7,15 @@ classdef FingerDataVizGui
         channel
         f_s
         fig
+        slider
+        edit_n
         from = 1
         to = 256
     end
     
     methods
-        function obj = FingerDataVizGui(train_data, train_dg, NFFT, ...
-                                        finger, channel, f_s)
+        function obj = FingerDataVizGui(train_data, train_dg, NFFT, finger, ...
+                                        channel, f_s)
             obj.train_data = train_data;
             obj.train_dg = train_dg;
             obj.NFFT = NFFT;
@@ -23,11 +25,15 @@ classdef FingerDataVizGui
             obj.fig = figure;
 
             maxval = size(train_data,1)/NFFT;
-            uicontrol('Style', 'slider',...
-                      'Min',1,'Max',maxval,'Value',1,...
-                      'Position', [20 10 1800 20],...
-                      'Callback', {@(src, event) set_time(obj, src, ...
-                                                               event)});           
+            obj.slider = uicontrol('Style', 'slider', 'Min',1,'Max',maxval, ...
+                                   'Value',1, 'Units', 'normalized', 'Position', ...
+                                   [0 0 1 .05], 'Callback', {@(src, event) ...
+                                obj.slider_callback( src, event)});
+            obj.edit_n = uicontrol('Style', 'edit', 'String','1', 'Units', 'normalized', ...
+                                   'Position', [0 .05 .1 .05], 'Callback', {@(src, event) ...
+                                obj.edit_n_callback(src, event)});
+            uicontrol('Style', 'text', 'String', num2str(maxval), 'Units', 'normalized', ...
+                      'Position', [0.9 .05 .1 .05]);
         end
         
         function paint_gui(obj)
@@ -41,11 +47,20 @@ classdef FingerDataVizGui
         
     end
     methods (Access=private)
-        function set_time(obj, src, event)
+        function edit_n_callback(obj, src, event)
+            str = get(src, 'String');
+            val = str2num(str);
+            obj.from = 1 + obj.NFFT*(val-1);
+            obj.to = obj.NFFT*val;
+            set(obj.slider, 'Value', val);
+            obj.paint_gui()
+        end
+                
+        function slider_callback(obj, src, event)
             val = int32(get(src, 'Value'));
             obj.from = 1 + obj.NFFT*(val-1);
             obj.to = obj.NFFT*val;
-    
+            set(obj.edit_n, 'String', val)
             obj.paint_gui()
         end
     end
