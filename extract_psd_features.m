0430f9071c904b6176dc28f3e270280fad3325dc
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-04-16 03:32:38 -0700

    Calculate mean beta/gamma PSDs.

---
+++ extract_psd_features.m
@@ -0,0 +1,53 @@
+%%
+clc;
+clear;
+close all;
+%%
+load('data/sub1_comp.mat');
+
+% parameters
+f_s = 1000;
+
+neighborhood = 4096; % Time area around a movement to sample data
+NFFT = 256;
+overlap = .875; % Percent overlap
+datalength = size(train_data, 1);
+samplesperhood = (neighborhood - NFFT*overlap)/(NFFT*(1-overlap));
+deltaN = NFFT * (1 - overlap);
+channel = 43;
+channels = [1 40 43 21 42 23]; % 
+timeoffset = 50; % Offset from movement to brain activity
+finger = 1;
+M = size(channels, 2); % number of features
+
+%%
+[pks, idxs] = findpeaks(train_dg(:, finger), 'MINPEAKHEIGHT', ...
+                        0.5, 'MINPEAKDISTANCE', neighborhood/2);
+numpeaks = size(pks,1);
+finger_movements = zeros(neighborhood, numpeaks);
+features = zeros(numpeaks*samplesperhood, M);
+betas = zeros(numpeaks*samplesperhood, M);
+gammas = zeros(numpeaks*samplesperhood, M);
+values = zeros(numpeaks*samplesperhood, 1);
+% calculating power spectrum for each period of motion
+T = zeros(1, numpeaks*samplesperhood);
+for k=1:numpeaks
+    from = idxs(k)-neighborhood/2;
+    to = idxs(k) + neighborhood/2-1;
+    
+    for i = 1:samplesperhood
+        beg = from + (i-1)*deltaN;
+        en = beg + NFFT - 1;
+        if(en > datalength)
+            break;
+        end
+        X = psdfeature(train_data(beg:en, :), f_s, channels);
+        n = (k-1)*samplesperhood + i;
+        % features(n, :) = X;
+        betas(n, :) = X(1, :);
+        gammas(n, :) = X(2, :);
+        t = beg + timeoffset;
+        values(n) = train_dg(t , finger);
+        T(n) = t;
+    end
+end
---
+++ psdfeature.m
@@ -0,0 +1,23 @@
+function X = psdfeature(timesegment, f_s, channels)
+% MULTIGAMMAFEATURE - Calculates a feature vector based on gamma
+% band power, multiple channels
+%  
+% timesegment is an N x M matrix, where M is the number of channels
+% and N is the length of the timeseries
+
+    bands = [12 40; 66 114];
+    numbands = size(bands, 1);
+    numchannels = size(channels, 2);
+    X = zeros(numbands, numchannels);
+    for k = 1:numchannels;
+        channel = channels(k);
+        NFFT = size(timesegment,1);
+        NW = 6;
+
+        bins = freqtobin(bands, f_s, NFFT);
+        psd = pmtm(timesegment(:, channel), NW);
+        for b = 1:numbands
+            X(b, k) = mean(psd(bins(b,1):bins(b,2))); %!!!!!!!!!!!!!!
+        end
+    end
+end
