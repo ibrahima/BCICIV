b6adb4da0ac6875469d63eb22ccdaab3e0db227b
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-06-03 14:29:18 -0700

    Update feature extraction bash script

--- batch_feature_extract.sh
+++ batch_feature_extract.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
-windows=( 40 80 100 120 )
-NWs=(1.5)
+windows=( 40 80 100 120 200 )
+NWs=(1.5 2 2.5 3 3.5 4 5 6)
 NFFT=256
 channels=":"
 datafile='data/sub1_comp'
-overlap=(0 0.5 0.75)
+overlaps=(0 0.5 0.75)
 
 for windowlen in "${windows[@]}"; do
     echo "Window Length=$windowlen"
-    for NW in "${NWs[@]}"; do
-        echo "NW=$NW"
-        for overlap in "${overlap[@]}"; do
+    for overlap in "${overlaps[@]}"; do
+        echo "overlap = $overlap"
+        for NW in "${NWs[@]}"; do
             matlab -nodisplay -nosplash -r "fx_gen('$datafile', $windowlen, $NW, 1000, $overlap, $NFFT, $channels);quit" &
+            # Wait on children so as not to overload the machine
+            wait
         done
-        # Wait on children so as not to overload the machine
-        wait
     done
+
 done
