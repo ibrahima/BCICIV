d3e7111d1a01a8816f2a84af742c8a1ca4e74407
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2014-06-03 20:56:42 -0700

    Limit concurrent tasks to the number of cores (rudimentary).

--- batch_feature_extract.sh
+++ batch_feature_extract.sh
@@ -6,16 +6,21 @@ NFFT=256
 channels=":"
 datafile='data/sub1_comp'
 overlaps=(0 0.5 0.75)
+corecount=4;
 
 for windowlen in "${windows[@]}"; do
     echo "Window Length=$windowlen"
     for overlap in "${overlaps[@]}"; do
         echo "overlap = $overlap"
+        c=0
         for NW in "${NWs[@]}"; do
+            (( c+= 1))
             matlab -nojvm -nodisplay -nosplash -r "fx_gen('$datafile', $windowlen, $NW, 1000, $overlap, $NFFT, $channels);quit" &
+            if ! (( c % corecount)); then
+                # Wait on children so as not to overload the machine
+                wait
+            fi
         done
-        # Wait on children so as not to overload the machine
-        wait
     done
 
 done
